9329cc9586351f668cf11c5259ac5024a0542e71
FJ-RATO <martinho.francisco@ua.pt>
2021-03-09 14:04:00 +0000

    removed logs

--- JS/color.js
+++ JS/color.js
@@ -13,29 +13,22 @@ const obeserver = new IntersectionObserver(function(entries,obeserver){
     entries.forEach(entry => {
       if(entry.isIntersecting && entry.target == sectionOne){
         document.body.style.backgroundColor = "#550385";
-        console.log("s1");
-      }
+              }
       if(entry.isIntersecting && entry.target == sectionTwo){
         document.body.style.backgroundColor = "#47026F";
-        console.log("s2");
-      }
+              }
       if(entry.isIntersecting && entry.target == sectionThree){
         document.body.style.backgroundColor = "#390259";
-        console.log("s3");
-      }
+              }
       if(entry.isIntersecting && entry.target == sectionFour){
         document.body.style.backgroundColor = "#2B0144";
-        console.log("s4");
-      }
+              }
       if(entry.isIntersecting && entry.target == sectionFive){
         document.body.style.backgroundColor = "#1D012E";
-        console.log("s5");
-      }
+              }
       if(entry.isIntersecting && entry.target == sectionSix){
         document.body.style.backgroundColor = "#0F0018";
-        console.log("s6");
-      }
-      console.log(entry.target)
+              }
     })
 },options);
 
