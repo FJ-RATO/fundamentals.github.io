a723e257bbda5dcc394ef839e4d6ec8adbc08b3f
FJ-RATO <martinho.francisco@ua.pt>
2021-03-09 13:31:00 +0000

    corrected the scroll fade

--- JS/color.js
+++ JS/color.js
@@ -11,36 +11,38 @@ const options = { };
 
 const obeserver = new IntersectionObserver(function(entries,obeserver){
     entries.forEach(entry => {
-      if(entry.target == sectionOne){
+      if(entry.isIntersecting && entry.target == sectionOne){
         document.body.style.backgroundColor = "#550385";
         console.log("s1");
       }
-      if(entry.target == sectionTwo){
+      if(entry.isIntersecting && entry.target == sectionTwo){
         document.body.style.backgroundColor = "#47026F";
         console.log("s2");
       }
-      if(entry.target == sectionThree){
+      if(entry.isIntersecting && entry.target == sectionThree){
         document.body.style.backgroundColor = "#390259";
         console.log("s3");
       }
-      if(entry.target == sectionFour){
+      if(entry.isIntersecting && entry.target == sectionFour){
         document.body.style.backgroundColor = "#2B0144";
         console.log("s4");
       }
-      if(entry.target == sectionFive){
+      if(entry.isIntersecting && entry.target == sectionFive){
         document.body.style.backgroundColor = "#1D012E";
         console.log("s5");
       }
-      if(entry.target == sectionSix){
+      if(entry.isIntersecting && entry.target == sectionSix){
         document.body.style.backgroundColor = "#0F0018";
         console.log("s6");
       }
+      console.log(entry.target)
     })
 },options);
 
-obeserver.observe(sectionOne);
-obeserver.observe(sectionTwo);
-obeserver.observe(sectionThree);
-obeserver.observe(sectionFour);
-obeserver.observe(sectionFive);
 obeserver.observe(sectionSix);
+obeserver.observe(sectionFive);
+obeserver.observe(sectionFour);
+obeserver.observe(sectionThree);
+obeserver.observe(sectionTwo);
+obeserver.observe(sectionOne);
+
